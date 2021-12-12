5b5e301e6ea8db8c863f325b50b7398738e276ad
fcmdhyd <69862604+fcmdhyd@users.noreply.github.com>
2021-12-12 13:48:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+#Write your code below this line 👇
+
+autogen = [rock,paper,scissors]
+
+print("Rock Paper Scissor Showdown!\n")
+
+me = int(input("What do you choose?\n\nType 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+if me >= 3 or me <0:
+  print("Oops! Wrong number!\n")
+else:
+  print(autogen[me])
+
+  print("Computer choose:\n")
+
+  cpugen = random.randint(0,2)
+  print(autogen[cpugen])
+  cpu = cpugen
+
+  if me > 2 or me < 0:
+    print("Oops! Did you type wrong?\n")
+  elif me == 0 and cpu == 2:
+    print("You Win!\n")
+  elif cpu == 1 and me == 0:
+    print("You lost!\n")
+  elif cpu > me:
+    print("You lose!\n")
+  elif me > cpu:
+    print("You win!\n")
+  elif me == cpu:
+    print("Draw!\n")
+
+  print("Thanks for playing!\n")
